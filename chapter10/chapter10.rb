135c253b551eb9cc2207be10d8b2def0417e9636
Akkkky <coding_addiction@yahoo.co.jp>
2020-11-04 22:09:35 +0900

    追加しきれていなかった10章の残りを追加

--- chapter10/chapter10.rb
+++ chapter10/chapter10.rb
@@ -1,16 +1,26 @@
-class WordSynth
-  def initialize
-    @effects = []
-  end
-    
-  def add_effect(effect)
-    @effects << effect
-  end
-    
-  def play(original_words)
-    @effects.inject(original_words) do |words,effect|
-    #効果を適用する
-      effect.call(words)
-    end
+# Procオブジェクト内で引数やローカル変数を参照すると、メソッドの実行が完了してもProcオブジェクトは引き続き引数やローカル変数にアクセスし続けることができる
+def generate_proc(array)
+  counter = 0
+  # Procオブジェクトをメソッドの戻り値とする
+  Proc.new do
+    # ローカル変数のcounterを加算する
+    counter += 10
+    # メソッド引数のarrayにcounterの値を追加する
+    array << counter
   end
 end
+
+values = []
+sample_proc = generate_proc(values)
+# generate_procメソッドを呼び出した直後はvaluesの中身は当然空のまま
+p values # => []
+
+# Procオブジェクトを実行するとgenerate_procメソッドの引数だったvaluesの中身が書き換えられる
+sample_proc.call
+p values # => [10]
+# generate_procメソッド内のローカル変数counterも加算され続ける
+sample_proc.call
+p values # => [10,20]
+
+# このようにメソッド（関数）が生成されたときのコンテキスト（変数情報など）を保持しているメソッドのことをプログラミング用語でクロージャ（closure、関数閉包）と言う
+# RubyのブロックやProcオブジェクトはクロージャとして振る舞う
